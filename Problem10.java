68e4fa0177293edb7ae69f7d9918ce1263541776
Afreen95 <afreenjuly@gmail.com>
2018-02-24 00:22:05 -0500

    Add files via upload

---
+++ Problem10.java
@@ -0,0 +1,29 @@
+
+public class Problem10 {
+	
+	 public static void main(String[] args){
+		 long sum = 0;
+		 int prime = 2;
+		 while (prime <= 2000000){
+			 if(isPrime(prime)){
+				 sum+= prime;
+			 }
+			 prime++;
+		 }
+		 System.out.println(sum); 
+	 }
+	 
+	 private static boolean isPrime(long a){
+		 if((a != 2)&& (a % 2 == 0)){
+			 return false;
+		 }
+		 for (long i = 3; i <= Math.sqrt(a); i += 2){
+			 if (a % i == 0){
+				 return false;
+				 }
+				 
+			 }
+		 return true;
+		 }
+	 }
+
